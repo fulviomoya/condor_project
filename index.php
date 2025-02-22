4518c58c6e8096822f8f42428c51cf3abdbb305e
ElGoat10 <carlosadrian1708@gmail.com>
2025-02-21 21:55:28 -0400

    modifique el icono de el index :)

--- index.php
+++ index.php
@@ -17,20 +17,19 @@
                 <img src="Parte_Administrativa\IMG\LOGO.png" alt="">
                 <h1>¡Bienvenido al <span>Politécnico ITLA!</span></h1>
                 <a href="Login.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                        class="icon icon-tabler icons-tabler-outline icon-tabler-user-circle">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                        <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                        <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-linecap="round" stroke-linejoin="round" width="36" height="36" stroke-width="1.55">
+                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                        <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                        <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
                     </svg>
                 </a>
             </div>
 
 
             <div class="instructions">
-                Para evitar inconvenientes, por favor, leer detenidamente todas las <span href="#">instrucciones</span> antes
+                Para evitar inconvenientes, por favor, leer detenidamente todas las <span href="#">instrucciones</span>
+                antes
                 de
                 llenar el formulario.
             </div>
@@ -46,7 +45,8 @@
                     </li>
                     <li>
                         <span class="field-name">Cédula:</span>
-                        <span class="field-description"> Ingresa el número de identificación del acta de nacimiento del alumno (O también llamada cédula) sin espacios ni guiones. </span>
+                        <span class="field-description"> Ingresa el número de identificación del acta de nacimiento del
+                            alumno (O también llamada cédula) sin espacios ni guiones. </span>
                     </li>
                     <li>
                         <span class="field-name">Escuela Anterior:</span>
