eb58ef754693564cf75ca4e769805f04cb9b084d
Ryte11 <luisangelgamer20@gmail.com>
2025-02-24 08:25:34 -0400

    actualizar funcion de generar plaza

--- Parte_Usuario/js/js_Form_oficial.js
+++ Parte_Usuario/js/js_Form_oficial.js
@@ -152,7 +152,7 @@ document.addEventListener('DOMContentLoaded', function () {
       if (index === sections.length - 1) {
         // Lógica para la última sección
         const generarIdPlaza = () => {
-          return `PL${new Date().getMilliseconds().toString()}`;
+          return `PL${String(Math.floor(Math.random() * 9000000) + 1000000)}`  + `${new Date().getHours().toString()}` + `${new Date().getMilliseconds().toString()}`;
         };
 
         const nombreCompleto = [
