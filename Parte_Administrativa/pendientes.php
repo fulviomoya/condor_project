df1f4f2795c599e276a77951e4aa89674bdaf653
Fulvio Andrés <fulvio.127@gmail.com>
2025-03-20 13:16:33 -0400

    pagination bar

--- Parte_Administrativa/pendientes.php
+++ Parte_Administrativa/pendientes.php
@@ -136,6 +136,14 @@ verificarSesion();
           </table>
         </div>
       </div>
+        <nav>
+          <ul class="pagination justify-content-end">
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Siguientes</a></li>
+          </ul>
+        </nav>
     </div>
   </div>
 
